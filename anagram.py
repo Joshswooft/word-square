573bdd6d6066400e27b2f618cf24b89a77061967
Josh Dando <josh@swooft.io>
2020-12-23 17:12:22 +0000

    fix: anagram wasnt taking into consideration the character count

--- anagram.py
+++ anagram.py
@@ -2,12 +2,26 @@
 from collections import Counter
 
 
+
+def contains(container, contained):
+    return all(container[x] >= contained[x] for x in contained)
+    
+    '''
+    Check that b is an anagram of a. 
+    A can be larger than b. 
+    E.g. a = house, b = hoe, res = True
+    '''
+def check_anagram(a: str, b:str)->bool:
+    if len(b) > len(a):
+        return False
+    return contains(Counter(a), Counter(b))
+
 def get_anagrams(n: int, letters: str)->list:
     assert (n >= 1 and n <= len(letters)), "Invalid anagram"
     
     anagrams = []
     with open("./words.txt") as f:
-        anagrams = [ word.strip() for word in f.readlines() if not set(word.strip()) - set(letters) and len(word.strip()) == n]
+        anagrams = [ word.strip() for word in f.readlines() if len(word.strip()) == n and check_anagram(letters, word.strip())]
 
     return anagrams
 
--- test_anagram.py
+++ test_anagram.py
@@ -1,7 +1,7 @@
 import unittest
 
-from anagram import get_anagrams
-
+from anagram import get_anagrams, check_anagram
+from collections import Counter
 
 class AnagramTestCase(unittest.TestCase):
     
@@ -13,6 +13,47 @@ class AnagramTestCase(unittest.TestCase):
     def test_invalid_square_length(self):
         with self.assertRaises(AssertionError):
             get_anagrams(100, "bar")
+
+    def test_anagrams(self):
+        n = 4
+        s = "door"
+        res = get_anagrams(n, s)
+        # Check that every word has the same count as the input
+        counts_s = Counter(s)
+        for word in res:
+            c = Counter(word)
+            for char in word:
+                if char not in counts_s or c[char] - counts_s[char] > 1 or len(s) != n:
+                    self.fail(word + " is not an anagram of " + s)
+
+    def test_check_anagram_valid(self):
+        a = "antler"
+        b = "rental"
+        res = check_anagram(a, b)
+        self.assertTrue(res)
+
+        a = "house"
+        b = "soe"
+        res = check_anagram(a, b)
+        self.assertTrue(res)
+
+
+    def test_check_anagram_invalid(self):
+        a = "door"
+        b = "dodo"
+        res = check_anagram(a, b)
+        self.assertFalse(res)
+
+        a = "beg"
+        b = "bee"
+
+        res = check_anagram(a, b)
+        self.assertFalse(res)
+
+        a = "foo"
+        b = "bar"
+        res = check_anagram(a, b)
+        self.assertFalse(res)
 
     def test_get_anagrams(self):
         n = 3
